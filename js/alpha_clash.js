c2999189538db4d39fb7aceb46ab6a57b98abb30
Hasan-Mahmud <hasanmiru@gmail.com>
2024-05-06 23:05:34 +0600

    add 2 js file alpha-clash.js and utility.js

---
+++ js/alpha_clash.js
@@ -0,0 +1,10 @@
+function play (){
+    // const homeScreen = document.getElementById('home');
+    // homeScreen.classList.add('hidden');
+
+    // const playGroundScreen = document.getElementById('play-ground');
+    // playGroundScreen.classList.remove('hidden');
+
+    hideElementById('home');
+    showElementById('play-ground');
+}
